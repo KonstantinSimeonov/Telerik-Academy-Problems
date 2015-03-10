d49716f97cd9e6ed48d7ef7616bfd7d8b8876050
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-03-10 18:14:14 +0200

    Defining classes part 1

---
+++ Defining-Classes-1/GSM/Call.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Call
+{
+    // PROPERTIES
+
+    public DateTime started { get; private set; }
+    public TimeSpan duration { get; private set; }
+    public string dialledNumber { get; private set; }
+
+    // CONSTRUCTORS
+
+    /// <summary>
+    /// Initializes an instance of the Call class with a number, a starting date and a duration.
+    /// </summary>
+    /// <param name="dialled"></param>
+    /// <param name="started"></param>
+    /// <param name="duration"></param>
+    public Call(string dialled, DateTime started, TimeSpan duration)
+    {
+        this.started = started;
+        this.duration = duration;
+        dialledNumber = dialled;
+    }
+
+    /// <summary>
+    /// Start a new call.
+    /// </summary>
+    /// <param name="dialled"></param>
+    public Call(string dialled)
+    {
+        started = DateTime.Now;
+        dialledNumber = dialled;
+    }
+
+    /// <summary>
+    /// Finished the current call and updates its duration.
+    /// </summary>
+    public void StopCall()
+    {
+        duration = started - DateTime.Now;
+    }
+
+    /// <summary>
+    /// Overload of the ToString() method inherited from System.Object.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return string.Format("Dialled number: {0} Dialled on: {1} Duration: {2}", dialledNumber, started, duration);
+    }
+}
+
---
+++ Defining-Classes-1/GSM/Display.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Display
+{
+    // FIELDS
+
+    private double? screenSize;
+    private int? numberOfColours;
+
+    // PROPERTIES
+
+    public double? ScreenSize 
+    {
+        get
+        {
+            return screenSize;
+        }
+        private set
+        {
+            try
+            {
+                screenSize = value;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Invalid argument provided for the screen size");
+            }
+        }
+    }
+
+    public int? NumberOfColours
+    {
+        get
+        {
+            return numberOfColours;
+        }
+        private set
+        {
+            try
+            {
+                numberOfColours = value;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Invalid argument provided for number of colours");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Initializes an instance of the Display class with size and number of colours.
+    /// </summary>
+    /// <param name="size"></param>
+    /// <param name="colourNum"></param>
+    public Display(double? size = null, int? colourNum = null)
+    {
+        this.ScreenSize = size;
+        this.NumberOfColours = colourNum;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("size: {0}, number of colours: {1}", ScreenSize, NumberOfColours);
+    }
+}
