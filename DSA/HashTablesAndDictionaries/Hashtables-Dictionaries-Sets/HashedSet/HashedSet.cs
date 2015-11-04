2152d718effcbe6e16506176cf0fce79d3cab342
KonstantinSimeonov <kon.simeonov@gmail.com>
2015-11-04 16:15:24 +0200

    Homework for hashsets and dictionaries

---
+++ DSA/HashTablesAndDictionaries/Hashtables-Dictionaries-Sets/HashedSet/HashedSet.cs
@@ -0,0 +1,129 @@
+﻿namespace HashtablesDictionariesSets.HashedSet
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using HashTable;
+
+    public class HashedSet<T> : IHashedSet<T>
+    {
+        private IDictionary<int, T> elements;
+
+        public HashedSet(IDictionary<int, T> foundation)
+        {
+            this.elements = foundation;
+        }
+
+        public HashedSet()
+            : this(new HashTable<int, T>())
+        {
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.elements.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return this.elements.IsReadOnly;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (!this.elements.Contains(new KeyValuePair<int, T>(item.GetHashCode(), item)))
+            {
+                this.elements.Add(item.GetHashCode(), item);
+            }
+        }
+
+        public void Clear()
+        {
+            this.elements.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return this.elements.ContainsKey(item.GetHashCode()) 
+                && this.elements[item.GetHashCode()].Equals(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            var iterator = this.elements.GetEnumerator();
+
+            while (arrayIndex < array.Length && iterator.MoveNext())
+            {
+                array[arrayIndex] = iterator.Current.Value;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.elements)
+            {
+                yield return item.Value;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            return this.elements.Remove(item.GetHashCode());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IHashedSet<T> UnionWith(params ICollection<T>[] sets)
+        {
+            var result = new HashedSet<T>();
+
+            foreach (var item in this)
+            {
+                if (!result.Contains(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            foreach (var set in sets)
+            {
+                foreach (var item in set)
+                {
+                    if (!result.Contains(item))
+                    {
+                        result.Add(item);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public IHashedSet<T> IntersectWith(params ICollection<T>[] sets)
+        {
+            var result = new HashedSet<T>();
+
+            foreach (var set in sets)
+            {
+                foreach (var item in set)
+                {
+                    if (this.Contains(item) && sets.All(s => s.Contains(item)))
+                    {
+                        result.Add(item);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DSA/HashTablesAndDictionaries/Hashtables-Dictionaries-Sets/OccurencesCounter.cs
@@ -0,0 +1,89 @@
+﻿namespace HashtablesDictionariesSets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class OccurencesCounter
+    {
+        private static readonly char[] Punctuation; // = new char[] { '.', ',', '!', ' ', '/', '-', ':', ';', '?', '"', '\'', '&', '–' };
+
+        static OccurencesCounter()
+        {
+            // lets generate some punctuation :D
+            var nonLetters = new List<char>();
+
+            for (int i = 0; i < 128; i++)
+            {
+                var codeAsChar = i.ToChar();
+                if(!codeAsChar.IsLetter() && !codeAsChar.IsDigit())
+                {
+                    nonLetters.Add(i.ToChar());
+                }
+            }
+
+            Punctuation = nonLetters.ToArray();
+        }
+
+        /// <summary>
+        /// Write a program that counts in a given array of double values the
+        /// number of occurrences of each value. Use Dictionary<TKey,TValue>.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static IDictionary<double, int> GetDoubleOccurrencesWithin(IEnumerable<double> numbers)
+        {
+            return GetOccurrencesCountsWithin(numbers);
+        }
+
+        /// <summary>
+        /// Write a program that extracts from a given sequence of strings all elements that present in it odd number of times.
+        /// </summary>
+        /// <param name="strings"></param>
+        /// <returns></returns>
+        public static IDictionary<string, int> GetOddOccurringElements(IEnumerable<string> strings)
+        {
+            var result = GetOccurrencesCountsWithin(strings)
+                                                .Where(o => o.Value % 2 == 1)
+                                                .ToDictionary(p => p.Key, p => p.Value);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Write a program that counts how many times each word from given text file words.txt appears in it. 
+        /// The character casing differences should be ignored. The result words should be ordered by their number of occurrences in the text. 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static IDictionary<string, int> GetOccurrencesFromText(string text)
+        {
+            var words = text.Split(Punctuation, StringSplitOptions.RemoveEmptyEntries)
+                                                                        .Select(w => w.ToLower())
+                                                                        .ToArray();
+
+            var result = GetOccurrencesCountsWithin(words)
+                                            .OrderBy(p => p.Value)
+                                            .ToDictionary(p => p.Key, p => p.Value);
+
+            return result;
+        }
+
+        private static IDictionary<T, int> GetOccurrencesCountsWithin<T>(IEnumerable<T> sequence)
+        {
+            var occurrences = new Dictionary<T, int>();
+
+            sequence.ForEach(x =>
+            {
+                if (!occurrences.ContainsKey(x))
+                {
+                    occurrences.Add(x, 0);
+                }
+
+                occurrences[x]++;
+            });
+
+            return occurrences;
+        }
+    }
+}
